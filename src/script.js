eb4af397749a6da353b176452d2cdf1ac39d6301
rych7 <rchum777@gmail.com>
2021-09-02 19:31:21 -0700

    mobile-responsive

--- src/script.js
+++ src/script.js
@@ -36,3 +36,47 @@ function ops_btn() {
 function ops_apply_btn() {
     window.open("https://forms.gle/MWch2AWkFiTdyf6v5", "_blank");
 }
+
+
+function darken_screen(yesno) {
+    if (yesno == true) {
+        document.querySelector('.screen-darken').classList.add('active');
+    }
+    else if (yesno == false) {
+        document.querySelector('.screen-darken').classList.remove('active');
+    }
+}
+
+function close_offcanvas() {
+    darken_screen(false);
+    document.querySelector('.mobile-offcanvas.show').classList.remove('show');
+    document.body.classList.remove('offcanvas-active');
+}
+
+function show_offcanvas(offcanvas_id) {
+    darken_screen(true);
+    document.getElementById(offcanvas_id).classList.add('show');
+    document.body.classList.add('offcanvas-active');
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.querySelectorAll('[data-trigger]').forEach(function (everyelement) {
+        let offcanvas_id = everyelement.getAttribute('data-trigger');
+        everyelement.addEventListener('click', function (e) {
+            e.preventDefault();
+            show_offcanvas(offcanvas_id);
+        });
+    });
+
+    document.querySelectorAll('.btn-close').forEach(function (everybutton) {
+        everybutton.addEventListener('click', function (e) {
+            close_offcanvas();
+        });
+    });
+
+    document.querySelector('.screen-darken').addEventListener('click', function (event) {
+        close_offcanvas();
+    });
+
+}); 
